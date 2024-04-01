1c1ed110cbf9de06889254f234c294c0c5596c30
Rabbit937 <hackrabbit@qq.com>
2024-04-01 15:56:07 +0800

    feat: 迁移Git

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -1,7 +1,7 @@
 // 主题独有配置
-import { getThemeConfig } from '@sugarat/theme/node';
+import { getThemeConfig } from '@sugarat/theme/node'
 
-// 开启RSS支持
+// 开启RSS支持（RSS配置）
 // import type { Theme } from '@sugarat/theme'
 
 // const baseUrl = 'https://sugarat.top'
@@ -14,12 +14,38 @@ import { getThemeConfig } from '@sugarat/theme/node';
 //   image: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
 //   favicon: 'https://sugarat.top/favicon.ico',
 // }
-// 详见文档: https://theme.sugarat.top/
+
+// 所有配置项，详见文档: https://theme.sugarat.top/
 const blogTheme = getThemeConfig({
   // 开启RSS支持
   // RSS,
+
+  // 搜索
+  // 默认开启pagefind离线的全文搜索支持（如使用其它的可以设置为false）
+  // 如果npx pagefind 时间过长，可以手动将其安装为项目依赖 pnpm add pagefind
+  // search: false,
+
+  // 页脚
+  footer: {
+    // message 字段支持配置为HTML内容，配置多条可以配置为数组
+    // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
+    copyright: 'MIT License | 粥里有勺糖',
+    // icpRecord: {
+    //   name: '蜀ICP备19011724号',
+    //   link: 'https://beian.miit.gov.cn/'
+    // },
+    // securityRecord: {
+    //   name: '公网安备xxxxx',
+    //   link: 'https://www.beian.gov.cn/portal/index.do'
+    // },
+  },
+
+  // 主题色修改
+  themeColor: 'el-blue',
+
   // 文章默认作者
-  author: 'hackrabbit',
+  author: '粥里有勺糖',
+
   // 友链
   friend: [
     {
@@ -37,38 +63,41 @@ const blogTheme = getThemeConfig({
       url: 'https://vitepress.dev/',
     },
   ],
-  // 开启离线的全文搜索支持（如构建报错可注释下面的配置再次尝试）
-  search: 'pagefind',
-  // popover: {
-  //   title: '公告',
-  //   body: [
-  //     {
-  //       type: 'text',
-  //       content:
-  //         'QQ交流群：681489336 🎉🎉',
-  //     },
-  //     {
-  //       type: 'text',
-  //       content:
-  //         '👇公众号👇---👇 微信 👇',
-  //     },
-  //     {
-  //       type: 'image',
-  //       src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210',
-  //     },
-  //     {
-  //       type: 'text',
-  //       content:
-  //         '欢迎大家加群&私信交流',
-  //     },
-  //     {
-  //       type: 'button',
-  //       content: '博客',
-  //       link: 'https://sugarat.top',
-  //     },
-  //   ],
-  //   duration: 0,
-  // },
-});
 
-export { blogTheme };
+  // 公告
+  popover: {
+    title: '公告',
+    body: [
+      { type: 'text', content: '👇公众号👇---👇 微信 👇' },
+      {
+        type: 'image',
+        src: 'https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210'
+      },
+      {
+        type: 'text',
+        content: '欢迎大家加群&私信交流'
+      },
+      {
+        type: 'text',
+        content: '文章首/文尾有群二维码',
+        style: 'padding-top:0'
+      },
+      {
+        type: 'button',
+        content: '作者博客',
+        link: 'https://sugarat.top'
+      },
+      {
+        type: 'button',
+        content: '加群交流',
+        props: {
+          type: 'success'
+        },
+        link: 'https://theme.sugarat.top/group.html',
+      }
+    ],
+    duration: 0
+  },
+})
+
+export { blogTheme }
